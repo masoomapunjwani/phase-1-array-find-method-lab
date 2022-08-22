a9f36bafa4f3adb310108dcb185cd86a2ab2ae87
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-22 20:53:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function winner(checkWinner){
+    return (checkWinner.result === "W");
+};
+
+function superbowlWin(array){
+    let win = array.find(winner);
+    if (typeof win === "object"){
+        return win.year;
+    } else{
+        return win;
+    }
+};
